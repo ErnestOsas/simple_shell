ded58da78a71799bc7fda7ad5f30210452ad03ec
Ernest Akibor <akiborernest@gmail.com>
2023-07-26 04:40:53 -0700

    free_grid

--- free_grid.c
+++ free_grid.c
@@ -1,13 +1,16 @@
-#include "shell.h"/**
+#include "shell.h"
+/**
  * free_grid - Free the allocated memory used
  * @grid: grid pointer
  * @height: Grid height
  */
 void free_grid(char **grid, int height)
 {
-        int i;        if (!grid)
-                return;
-        for (i = 0; i < height; i++)
-                free(grid[i]);
-        free(grid);
+int i;
+
+if (!grid)
+return;
+for (i = 0; i < height; i++)
+free(grid[i]);
+free(grid);
 }
